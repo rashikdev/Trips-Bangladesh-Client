a06a944c1114caa205ad1b0279c9f02375ea3b0c
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-07-11 10:30:24 +0600

    implement update tourist profile functionality

--- src/Profile/TouristProfile/TouristProfile.jsx
+++ src/Profile/TouristProfile/TouristProfile.jsx
@@ -3,22 +3,39 @@ import { useNavigate } from "react-router";
 import useAuth from "../../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { getImgUrl } from "../../utils/utils";
+import { set } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const TouristProfile = () => {
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
   const axiosSecure = useAxiosSecure();
   const [editModal, setEditModal] = useState(false);
-  const [formData, setFormData] = useState({
-    name: user?.displayName || "",
-    photoURL: user?.photoURL || "",
-  });
-
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [saving, setSaving] = useState(false);
   const navigate = useNavigate();
+
+  const handleUpload = async (e) => {
+    const image = await getImgUrl(e.target.files[0]);
+    setUploadedImage(image);
+  };
 
   const handleUpdate = (e) => {
     e.preventDefault();
-    console.log("Updated Data:", formData);
-    setEditModal(false);
+    setSaving(true);
+    const form = e.target;
+    const name = form.name.value || user?.displayName;
+    const photoURL = uploadedImage || user?.photoURL;
+    updateUser({
+      displayName: name,
+      photoURL: photoURL,
+    })
+      .then(() => {
+        setSaving(false);
+        toast.success("Profile updated successfully");
+        setEditModal(false);
+      })
+      .catch((err) => toast.error("something went wrong try again"));
   };
 
   const { data: application = {} } = useQuery({
@@ -102,7 +119,7 @@ const TouristProfile = () => {
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 backdrop-blur-sm px-4">
           <form
             onSubmit={handleUpdate}
-            className="bg-white text-black p-8 rounded-2xl w-full max-w-md shadow-xl space-y-5"
+            className="bg-white/10 border border-white/20 p-8 rounded-2xl w-full max-w-md shadow-xl space-y-5"
           >
             <h3 className="text-2xl font-bold text-center mb-2">
               Edit Your Profile
@@ -112,41 +129,36 @@ const TouristProfile = () => {
               <label className="block text-sm font-medium mb-1">Name</label>
               <input
                 type="text"
-                value={formData.name}
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, name: e.target.value }))
-                }
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-400"
+                name="name"
+                defaultValue={user?.displayName}
+                className="w-full px-4 py-2 border border-gray-500 rounded-md focus:outline-none"
               />
             </div>
 
             <div>
-              <label className="block text-sm font-medium mb-1">
-                Photo URL
-              </label>
+              <label className="block text-sm font-medium mb-1">Photo</label>
               <input
-                type="text"
-                value={formData.photoURL}
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, photoURL: e.target.value }))
-                }
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-400"
+                type="file"
+                name="photo"
+                accept="image/*"
+                onChange={handleUpload}
+                className="w-full px-3 py-1 rounded-md  border border-white/30 text-white file:text-white file:bg-teal-500 file:border-none file:px-4 file:py-1 file:rounded file:cursor-pointer focus:outline-none"
               />
             </div>
 
-            <div className="flex justify-end gap-3 pt-4">
+            <div className="flex justify-end gap-3 pt-4 font-semibold">
               <button
                 type="button"
                 onClick={() => setEditModal(false)}
-                className="px-4 py-2 rounded border hover:bg-gray-100"
+                className="px-4 py-2 rounded border border-gray-400 hover:border-orange-500 text-sm transition cursor-pointer"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                className="px-4 py-2 rounded bg-purple-600 text-white hover:bg-purple-700"
+                className="px-4 py-2 rounded bg-white text-black cursor-pointer"
               >
-                Save Changes
+                {saving ? "Saving..." : "Save Changes"}
               </button>
             </div>
           </form>
